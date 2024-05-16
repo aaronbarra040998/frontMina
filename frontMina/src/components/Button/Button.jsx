b6529ef32fa2b74375969612a36a9c5a18114891
Aaron A. Barra <121281535+aaronbarra040998@users.noreply.github.com>
2024-05-15 21:30:59 -0500

    frontstructure

---
+++ frontMina/src/components/Button/Button.jsx
@@ -0,0 +1,8 @@
+// Button.jsx
+import './Button.css'; // Importa el archivo de estilos CSS
+
+export default function Button() {
+  return (
+    <button className="button">Click Me</button>
+  );
+}
---
+++ frontMina/src/pages/Home/index.jsx
@@ -0,0 +1,10 @@
+import Button from "../../components/Button/Button";
+
+export default function Home() {
+    return (
+        <div>
+        <h1>Home</h1>
+        <Button />
+        </div>
+    );
+    }
